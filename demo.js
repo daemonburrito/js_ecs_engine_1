714ffc6184cb3162319b5b40f71baffbe9164b40
Scott Drake <demonburrito@gmail.com>
2014-08-11 18:25:35 -0500

    Bouncing square working.
    * Canvas rendering.
    * fn pipelines working.
    * npm stuff.

--- demo.js
+++ demo.js
@@ -1,9 +1,45 @@
 (function () {
 	"use strict";
-	var main = require('./main.js');
+	var main = require('./main.js'),
+		canvas = document.getElementById('demo'),
+		ctx = canvas.getContext('2d');
 
-	main([function () {
-		console.log('tick');
-		console.log(this.dt);
+	ctx.fillStyle = '#FF0000';
+	ctx.strokeStyle = '#000000';
+
+	var x = 0, y = 0,
+		forward = true;
+
+	ctx.fillRect(x, y, 10, 10);
+
+	var sync_fns = [
+		function (cb) {
+			ctx.clearRect(0, 0, 300, 300);
+			cb();
+		},
+		function (cb) {
+			if (x === 0) {
+				forward = true;
+			}
+
+			if (x < 290 && forward === true) {
+				x += 1;
+				y += 1;
+			}
+			else {
+				forward = false;
+				x -= 1;
+				y -= 1;
+			}
+
+			ctx.fillRect(x, y, 10, 10);
+
+			cb();
+		}
+	];
+
+	main(sync_fns, [function () {
+		//console.log('tick');
+		//console.log('dt', this.dt);
 	}]);
 })();
--- main.js
+++ main.js
@@ -1,41 +1,45 @@
 // main module
 // game loop, asset loading, etc
-var main = function (pipeline) {
+var main = function (pipeline, async_pipeline) {
 	"use strict";
-	var now, dt, last, ctx,
+	var now, dt, last, context,
+		async = require('async'),
+
+		// raf cb
 		frame = function () {
+
 			now = timestamp();
 			dt = (now - last) / 1000,
 
-			ctx = {
+			context = {
 				dt: dt
 			};
 
-			pipeline.forEach(function (fn) {
-				fn.call(ctx);
+			async.series(pipeline.map(function (fn) {
+				return fn.bind(context);
+			}));
+
+			// async pipeline
+			// calls in this queue do not need a next(), but will not block eachother.
+			async_pipeline.forEach(function (fn) {
+				fn.call(context);
 			}, this);
 
 			last = now;
+
+			// next frame
 			requestAnimationFrame(frame);
 		},
 
+		// wrapper for clock
 		timestamp = function () {
 			return window.performance.now();
 		};
 
 	last = timestamp();
 
+	// starts the raf loop
 	requestAnimationFrame(frame);
 };
 
 module.exports = main;
-
-/*
-main([function () {
-	console.log('tick');
-	console.log(this.dt);
-}, function () {
-	console.log('tock');
-	console.log(this.dt);
-}]);
-*/
